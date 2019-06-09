6a96bbc524f18f5c5d15c2255b4ac887b506d11e
Mika Bettanin <mauro.bettanin@ikea.com>
2019-06-09 10:42:07 +0200

    Cheers

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -1,2 +1,36 @@
 class IngredientsController < ApplicationController
+  before_action :set_ingredient, only: [:show, :edit, :update, :destroy]
+  def index
+    @ingredients = Ingredient.all.order(:name)
+  end
+
+  def show
+  end
+
+  def new
+    @ingredients = Ingredient.all.order(:name)
+    @ingredient = Ingredient.new
+  end
+
+  def create
+    @ingredient = Ingredient.new(ingredient_params)
+    @ingredient.save
+    @ingredients = Ingredient.order(:name)
+    @ingredient.save
+    #  redirect_to 'cocktails/ingredients/new' #ingredients_path(@ingredient)
+    #else
+    #  render 'cocktails/ingredients'
+
+    #end
+  end
+
+  private
+
+  def ingredient_params
+    params.require(:ingredient).permit(:name)
+  end
+
+  def set_ingredient
+    @ingredient = Ingredient.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'cocktails/ingredients/new', to: 'ingredients#new'
+  get 'cocktails/ingredients', to: 'ingredients#index'
+  get '/ingredients', to: 'ingredients#index'
+  post '/ingredients', to: 'ingredients#create'
+  patch 'cocktails/:id', to: 'cocktails#create'
   resources :cocktails, only: [:index, :show, :new, :create] do
     resources :doses, only: [:new, :create]
   end
   post '/new_ingredient', to: 'cocktails#new_ingredient'
   resources :doses, only: [:destroy]
+  # resources :ingredients, only: [:index, :show, :new, :create]
 end
